2b7f9057765a50f21a0e10173854c6db774931a8
Aruanangshu Biswas <20253878+dotslash21@users.noreply.github.com>
2018-07-12 14:14:52 +0530

    Fixed Institute State Dropdown

--- website-files/tpo.php
+++ website-files/tpo.php
@@ -170,7 +170,15 @@
             <td>11. Institute State :</td>
            <td><select name="ins_st" id="dsts">
                 <?php 
-                $a=['Delhi','Westbengal','Name-3','Name-4','Name-5','Name-6','Name-7','Name-8'];
+                $a=['Andaman and Nicobar Islands(AN)', 'Andhra Pradesh (AP)', 'Arunachal Pradesh (AR)', 
+                    'Assam (AS)', 'Bihar (BR)', 'Chandigarh (CH)', 'Chhattisgarh (CG)', 
+                    'Dadra and Nagar Haveli (DN)', 'Daman and Diu (DD)', 'Goa (GA)', 'Gujarat (GJ)', 
+                    'Haryana (HR)', 'Himachal Pradesh (HP)', 'Jammu and Kashmir (JK)', 'Jharkhand (JH)', 
+                    'Karnataka (KA)', 'Kerala (KL)', 'Lakshadweep (LD)', 'Madhya Pradesh (MP)', 
+                    'Maharashtra (MH)', 'Manipur (MN)', 'Meghalaya (ML)', 'Mizoram (MZ)', 'Nagaland (NL)', 
+                    'National Capital Territory of Delhi (DL)', 'Odisha(OR)', 'Pondicherry (PY)', 
+                    'Punjab (PB)', 'Rajasthan (RJ)', 'Sikkim (SK)', 'Tamil Nadu (TN)', 'Telangana (TS)', 
+                    'Tripura (TR)', 'Uttar Pradesh (UP)', 'Uttarakhand (UK)', 'West Bengal (WB)'];
                 for( $i = 0; $i<count($a);$i++ ){
                     echo "<option value='$a[$i]'>$a[$i]</option>";};?>
 	           </select>
