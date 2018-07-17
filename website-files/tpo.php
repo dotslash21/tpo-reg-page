0d5d0d68b901908fe6827ec66e1e16f66e25774d
ANSHUMALI SHAW <37397899+ankugolu@users.noreply.github.com>
2018-07-17 22:10:40 +0530

    attach with php

--- website-files/tpo.php
+++ website-files/tpo.php
@@ -1,3 +1,59 @@
+<?php
+session_start();
+define("DBHOST","localhost");
+define("DBUSERNAME","root");
+define("DBPASSWORD","");
+define("DB","cpc_tpo");
+$con = mysqli_connect(DBHOST,DBUSERNAME,DBPASSWORD,DB);
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
+    $name=$_POST['name'];
+	$inst_code=$_POST['inst_code'];
+    $uid=$_POST['uid'];
+    $password=$_POST['password'];
+    $estd=$_POST['estd'];
+    $accrd=$_POST['accrd'];
+    $inst_type=$_POST['inst_type'];
+    $affli=$_POST['affli'];
+    $inst_appr=$_POST['inst_appr'];
+    $address=$_POST['address'];
+    $pin=$_POST['pin'];
+    $inst_state=$_POST['inst_state'];
+    $ins_dst=$_POST['ins_dst'];
+    $number=$_POST['number'];
+    $email=$_POST['email'];
+    $website=$_POST['website'];
+    $head_name=$_POST['head_name'];
+    $head_desg=$_POST['head_desg'];
+	$head_mob=$_POST['head_mob'];
+	$head_ph=$_POST['head_ph'];
+	$head_email=$_POST['head_email'];
+	$tpo_name=$_POST['tpo_name'];
+	$tpo_contact1=$_POST['tpo_contact1'];
+	$tpo_contact2=$_POST['tpo_contact2'];
+	$tpo_email=$_POST['tpo_email'];
+	$num_cmp=$_POST['num_cmp'];
+	$num_cmplab=$_POST['num_cmplab'];
+	$min_num_cmp=$_POST['min_num_cmp'];
+	$ispeed=$_POST['ispeed'];
+	$hall_cap=$_POST['hall_cap'];
+	$num_cctv=$_POST['num_cctv'];
+	$has_fiber=$_POST['has_fiber'];
+	
+	$sql = "Insert into creds(inst_name,inst_code,uid,pwd,estd,inst_accrd,inst_type,inst_affl,inst_aprv,state,district,pin,address,phone,email,website,head_name,
+	inst_headdesg,head_contact,head_mob,head_email,tpo_name,tpo_ph,tpo_ph2,tpo_email,no_of_comp,num_cmplab,min_num_cmp,int_speed,hall_cap,fibop_lan,cctv_no)values(
+	'".$name."','".$inst_code."','".$uid."','".$password."','".$estd."','".$accrd."','".$inst_type."','".$affli."','".$inst_appr."','".$inst_state."','".$ins_dst."',
+	'".$pin."','".$address."','".$number."','".$email."','".$website."','".$head_name."','".$head_desg."','".$head_ph."','".$head_mob."','".$head_email."','".$tpo_name."',
+	'".$tpo_contact1."','".$tpo_contact2."','".$tpo_email."','".$num_cmp."','".$num_cmplab."','".$min_num_cmp."','".$ispeed."','".$hall_cap."','".$num_cctv."',
+	'".$has_fiber."')";
+	mysqli_query($con,$sql);
+	echo "Successfully Inserted data!";
+}
+?>
+
+
+
+
 <!DOCTYPE html>
 <html>
 
@@ -31,10 +87,14 @@
 
     <!-- MAIN FORM BODY-->
     <div class="container z-depth-3" id="form-container">
-        <form>
+        <form method="POST">
             <div class="input-field">
                 <input type="text" id="name" name="name">
                 <label class="active" for="name">Institute Name</label>
+            </div>
+			<div class="input-field">
+                <input type="number" id="inst_code" name="inst_code">
+                <label class="active" for="inst_code">Institute code</label>
             </div>
             <div class="input-field">
                 <input type="text" id="uid" name="uid">
@@ -50,7 +110,7 @@
             </div>
 
             <div class="input-field">
-                <select>
+                <select name="accrd">
                     <option value="" disabled selected>Choose your option</option>
                     <option value="NAAC-A+">NAAC-A+</option>
                     <option value="NAAC-A">NAAC-A</option>
@@ -62,7 +122,7 @@
             </div>
 
             <div class="input-field">
-                <select>
+                <select name="inst_type">
                     <option value="" disabled selected>Choose your option</option>
                     <option value="Government">Government</option>
                     <option value="Government-Aided">Government-Aided</option>
@@ -72,7 +132,7 @@
             </div>
 
             <div class="input-field">
-                <select>
+                <select name="affli">
                     <option value="" disabled selected>Choose your option</option>
                     <option value="MAKAUT">Maulana Kalam Azad University of Technology</option>
                     <option value="CU">Calcutta University</option>
@@ -83,7 +143,7 @@
             </div>
 
             <div class="input-field">
-                <select>
+                <select name="inst_appr">
                     <option value="" disabled selected>Choose your option</option>
                     <option value="1">SELECT-1</option>
                     <option value="2">SELECT-2</option>
@@ -103,26 +163,34 @@
             </div>
 
             <div class="input-field">
-                <select>
-                    <option value="" disabled selected>Choose your option</option>
-                    <option value="1">SELECT-1</option>
-                    <option value="2">SELECT-2</option>
-                    <option value="3">SELECT-3</option>
+                <select name="inst_state">
+                <?php 
+                $a=['Andaman and Nicobar Islands(AN)', 'Andhra Pradesh (AP)', 'Arunachal Pradesh (AR)', 
+                    'Assam (AS)', 'Bihar (BR)', 'Chandigarh (CH)', 'Chhattisgarh (CG)', 
+                    'Dadra and Nagar Haveli (DN)', 'Daman and Diu (DD)', 'Goa (GA)', 'Gujarat (GJ)', 
+                    'Haryana (HR)', 'Himachal Pradesh (HP)', 'Jammu and Kashmir (JK)', 'Jharkhand (JH)', 
+                    'Karnataka (KA)', 'Kerala (KL)', 'Lakshadweep (LD)', 'Madhya Pradesh (MP)', 
+                    'Maharashtra (MH)', 'Manipur (MN)', 'Meghalaya (ML)', 'Mizoram (MZ)', 'Nagaland (NL)', 
+                    'National Capital Territory of Delhi (DL)', 'Odisha(OR)', 'Pondicherry (PY)', 
+                    'Punjab (PB)', 'Rajasthan (RJ)', 'Sikkim (SK)', 'Tamil Nadu (TN)', 'Telangana (TS)', 
+                    'Tripura (TR)', 'Uttar Pradesh (UP)', 'Uttarakhand (UK)', 'West Bengal (WB)'];
+                for( $i = 0; $i<count($a);$i++ ){
+                    echo "<option value='$a[$i]'>$a[$i]</option>";};?>
                 </select>
                 <label>Institute State</label>
             </div>
 
             <div class="input-field">
-                <!--
-                    <select>
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="1">SELECT-1</option>
-                        <option value="2">SELECT-2</option>
-                        <option value="3">SELECT-3</option>
-                    </select>
-                -->
-                <input type="text" name="district" id="district">
-                <label class="active" for="district">Institute District</label>
+            <select name="ins_dst" id="ins_dst">
+				<?php 
+					$a=['Alipurduar', 'Bankura', 'Birbhum', 'Cooch Behar', 'Dakshin Dinajpur','Darjeeling', 'Hooghly', 'Howrah', 'Jalpaiguri', 'Jhargram', 
+					'Kalimpong','Kolkata','Malda', 'Murshidabad', 'Nadia', 'North 24 Parganas', 'Paschim Bardhaman','Paschim Medinipur', 'Purba Bardhaman',
+					 'Purba Medinipur', 'Purulia', 'South 24 Parganas','Uttar Dinajpur'];
+				for( $i = 0; $i<count($a);$i++ ){
+				echo "<option value='$a[$i]'>$a[$i]</option>";};
+				?>
+	           </select>
+                <label for="ins_dst">Institute District</label>
             </div>
 
             <div class="input-field">
@@ -209,8 +277,8 @@
             </div>
 
             <div class="input-field">
-                <input type="number" id="num_cmp" name="num_cmp">
-                <label class="active" for="num_cmp">Institute Total Hall Capacity</label>
+                <input type="number" id="hall_cap" name="hall_cap">
+                <label class="active" for="hall_cap">Institute Total Hall Capacity</label>
             </div>
 
             <div class="input-field">
@@ -239,7 +307,7 @@
     <footer class="page-footer blue darken-3">
         <div class="footer-copyright blue darken-4">
             <div class="container">
-                Copyright Â© 2018. CPC, West Bengal
+                Copyright © 2018. CPC, West Bengal
                 <a class="grey-text text-lighten-4 right" href="http://gcettb.ac.in/home">Designed at GCETTB</a>
             </div>
         </div>
