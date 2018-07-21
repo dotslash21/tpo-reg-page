4340fbf185684e4f1876e3c472673a596ced4815
Arunangshu Biswas <20253878+dotslash21@users.noreply.github.com>
2018-07-21 14:00:32 +0530

    Fatal Commit Error Recovery

--- website-files/submit.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo '<h1>Form Submission System is still under constrution!</h1><hr>';
-    echo 'Redirecting to previous page in 3 seconds!';
-    header('Refresh: 3; URL=localhost:80/tpo.php');
-?>
