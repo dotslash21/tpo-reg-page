f5a83e14c9ee9ead6b38a197a65b1b6bf985b089
Arunangshu Biswas <20253878+dotslash21@users.noreply.github.com>
2018-07-23 21:34:00 +0530

    Minor fix

--- website-files/tpo.php
+++ website-files/tpo.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
     <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <style>
         #form-container {
