562add166cb81a16452f22b0322277bbf4945cb0
Arkadip <in2arkadipb13@gmail.com>
2018-07-26 21:15:37 +0530

    escaping

--- website-files/ajax/register.php
+++ website-files/ajax/register.php
@@ -62,6 +62,40 @@
             return mysqli_real_escape_string($con, $var);
         }
 
+        //escaping college cred
+        $name_clean         = clean($con, $name);
+        $inst_code_clean    = clean($con, $inst_code);
+        $uid_clean          = clean($con, $uid);
+        $password_clean     = clean($con, $password);
+        $estd_clean         = clean($con, $estd);
+        $accrd_clean        = clean($con, $accrd);
+        $inst_type_clean    = clean($con, $inst_type);
+        $affli_clean        = clean($con, $affli);
+        $inst_appr_clean    = clean($con, $inst_appr);
+        $address_clean      = clean($con, $address);
+        $pin_clean          = clean($con, $pin);
+        $inst_state_clean   = clean($con, $inst_state);
+        $ins_dst_clean      = clean($con, $ins_dst);
+        $number_clean       = clean($con, $number);
+        $email_clean        = clean($con, $email);
+        $website_clean      = clean($con, $website);
+        $head_name_clean    = clean($con, $head_name);
+        $head_desg_clean    = clean($con, $head_desg);
+        $head_mob_clean     = clean($con, $head_mob);
+        $head_ph_clean      = clean($con, $head_ph);
+        $head_email_clean   = clean($con, $head_email);
+        $tpo_name_clean     = clean($con, $tpo_name);
+        $tpo_contact1_clean = clean($con, $tpo_contact1);
+        $tpo_contact2_clean = clean($con, $tpo_contact2);
+        $tpo_email_clean    = clean($con, $tpo_email);
+        $num_cmp_clean      = clean($con, $num_cmp);
+        $num_cmplab_clean   = clean($con, $num_cmplab);
+        $min_num_cmp_clean  = clean($con, $min_num_cmp);
+        $ispeed_clean       = clean($con, $ispeed);
+        $hall_cap_clean     = clean($con, $hall_cap);
+        $num_cctv_clean     = clean($con, $num_cctv);
+        $has_fiber_clean    = clean($con, $has_fiber);
+
         //Check is already exist
         $query_usr = "SELECT inst_code FROM cred WHERE inst_code ='".$inst_code."' LIMIT 1";
         $result_usr =  mysqli_query($con,$query_usr);
@@ -72,14 +106,15 @@
         else{
             //If not exists
             
-            $query_reg = "INSERT INTO cred(inst_name,       inst_code,      uid,        pwd,        estd,       inst_accrd,         inst_type,      inst_affl,      inst_aprv,      state,          district,       pin,        address,        phone,      email,      website,        head_name,      inst_headdesg,  head_contact,   head_mob,       head_email,         tpo_name,       tpo_ph,         tpo_ph2,                tpo_email,      no_of_comp,     num_cmplab,     min_num_cmp,        int_speed,      hall_cap,     fibop_lan,        cctv_no)
-                                     VALUE('".$name."','".$inst_code."','".$uid."','".$password."','".$estd."','".$inst_accrd."','".$inst_type."','".$affli."','".$inst_appr."','".$inst_state."','".$ins_dst."','".$pin."','".$address."','".$number."','".$email."','".$website."','".$head_name."','".$head_desg."','".$head_ph."','".$head_mob."','".$head_email."','".$tpo_name."','".$tpo_contact1."','".$tpo_contact2."','".$tpo_email."','".$num_cmp."','".$num_cmplab."','".$min_num_cmp."','".$ispeed."','".$hall_cap."','".$has_fiber."','".$num_cctv."') ";
+            $query_reg = "INSERT INTO cred(     inst_name,       inst_code,             uid,                pwd,                estd,               inst_accrd,         inst_type,              inst_affl,          inst_aprv,              state,                  district,           pin,            address,            phone,              email,              website,            head_name,              inst_headdesg,          head_contact,       head_mob,               head_email,             tpo_name,               tpo_ph,                 tpo_ph2,                tpo_email,              no_of_comp,         num_cmplab,             min_num_cmp,            int_speed,          hall_cap,               fibop_lan,              cctv_no)
+                                     VALUE('".$name_clean."','".$inst_code_clean."','".$uid_clean."','".$password_clean."','".$estd_clean."','".$inst_accrd_clean."','".$inst_type_clean."','".$affli_clean."','".$inst_appr_clean."','".$inst_state_clean."','".$ins_dst_clean."','".$pin_clean."','".$address_clean."','".$number_clean."','".$email_clean."','".$website_clean."','".$head_name_clean."','".$head_desg_clean."','".$head_ph_clean."','".$head_mob_clean."','".$head_email_clean."','".$tpo_name_clean."','".$tpo_contact1_clean."','".$tpo_contact2_clean."','".$tpo_email_clean."','".$num_cmp_clean."','".$num_cmplab_clean."','".$min_num_cmp_clean."','".$ispeed_clean."','".$hall_cap_clean."','".$has_fiber_clean."','".$num_cctv_clean."') ";
+            
             if(mysqli_query($con,$query_reg)){
                 //first college data stored
 
                 //Looping for Course data
                 for($i = 0; $i < $course_length; $i ++){
-                    
+
                     //escaping
                     $course_name_clean = clean($con, $course_name[$i]);
                     $course_value_clean = clean($con, $course_value[$i]);
