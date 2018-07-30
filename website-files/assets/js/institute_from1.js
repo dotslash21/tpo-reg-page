ad549b9817ea4be8ad04148c65f5e86b778fc35e
Arkadip <in2arkadipb13@gmail.com>
2018-07-30 20:27:21 +0530

    Better href to form2 is added in form1

--- website-files/assets/js/institute_from1.js
+++ website-files/assets/js/institute_from1.js
@@ -172,8 +172,7 @@ $(document).on("submit","form.frm",function(event) {
         alert("Turn on Cookies");
         return false;
     }
-    console.log(sessionStorage);
-    if(sessionStorage.length == 16){
+    if(sessionStorage.name !== undefined  && sessionStorage.inst_code !== undefined  && sessionStorage.uid !== undefined  && sessionStorage.password !== undefined  && sessionStorage.estd !== undefined  && sessionStorage.accrd !== undefined  && sessionStorage.inst_type !== undefined  && sessionStorage.affli !== undefined  && sessionStorage.inst_appr !== undefined  && sessionStorage.address !== undefined  && sessionStorage.pin !== undefined  && sessionStorage.inst_state !== undefined  && sessionStorage.ins_dst !== undefined  && sessionStorage.number !== undefined  && sessionStorage.email !== undefined  && sessionStorage.website !== undefined){
         //Redirect Location
         window.location = './form2.php';
     }
