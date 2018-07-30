ba392cc43eaa7c509344005cf423168b8e54319e
Arkadip <in2arkadipb13@gmail.com>
2018-07-30 23:38:48 +0530

    again little housekeeping

--- website-files/ajax/register.php
+++ website-files/ajax/register.php
@@ -116,7 +116,7 @@
             //If not exists
             
             $query_reg = "INSERT INTO cred(inst_name, inst_code, uid, pwd, estd, inst_accrd, inst_type, inst_affl, inst_aprv, state, district, pin, address, phone, email, website, head_name, inst_headdesg, head_contact, head_mob, head_email, tpo_name, tpo_ph, tpo_ph2, tpo_email, no_of_comp, num_cmplab, min_num_cmp, int_speed, hall_cap, fibop_lan, cctv_no) VALUE('".$name_clean."','".$inst_code_clean."','".$uid_clean."','".$password_clean."','".$estd_clean."','".$accrd_clean."','".$inst_type_clean."','".$affli_clean."','".$inst_appr_clean."','".$inst_state_clean."','".$ins_dst_clean."','".$pin_clean."','".$address_clean."','".$number_clean."','".$email_clean."','".$website_clean."','".$head_name_clean."','".$head_desg_clean."','".$head_ph_clean."','".$head_mob_clean."','".$head_email_clean."','".$tpo_name_clean."','".$tpo_contact1_clean."','".$tpo_contact2_clean."','".$tpo_email_clean."','".$num_cmp_clean."','".$num_cmplab_clean."','".$min_num_cmp_clean."','".$ispeed_clean."','".$hall_cap_clean."','".$has_fiber_clean."','".$num_cctv_clean."') ";
-            $return['sql1'] = $query_reg;
+
             if(mysqli_query($con,$query_reg)){
                 //first college data stored
                 //Looping for Course data
@@ -132,7 +132,6 @@
                         $query_crs = '';
                         $query_crs .= 'INSERT INTO college_crs(college_id, deg_optd, intake) VALUES("'.$inst_code_clean.'", "'.$course_name_clean.'", "'.$course_value_clean.'") ';
                         if($query_crs != ''){
-                            $return['message3'] = "couse query ok";
                             if(mysqli_multi_query($con, $query_crs)){
 
                                 //return successful statements
@@ -140,9 +139,7 @@
                                 $return['redirect'] = './done.php';
 
                                 //add a session for file upload
-                                // $_SESSION['inst_code'] =(int)$inst_code;
-                                // $return['1'] = $inst_code;
-                                // $return['2'] = $_SESSION['inst_code'];
+                                $_SESSION['inst_code'] =(int)$inst_code;
                             }
                             else{
                                 $return['error'] = 'Putting course data in the DataBase is Failed. Try again later';
