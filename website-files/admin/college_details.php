42f243ad5cfcea395632376f2d5cacdf03afe230
Arunangshu Biswas <20253878+dotslash21@users.noreply.github.com>
2018-07-31 23:10:39 +0530

    Multiple files fix

--- website-files/admin/college_details.php
+++ website-files/admin/college_details.php
@@ -33,10 +33,6 @@
             margin: 0.2em;
         }
 
-        .panel_options {
-            margin-left: auto;
-        }
-
         body {
             display: flex;
             min-height: 100vh;
@@ -85,61 +81,108 @@
                 </div>
             </div>
 
-            <ul class="collapsible popout" data-collapsible="expandable">
-                <?php
-                    $sql_clg = "SELECT inst_name,inst_code FROM cred ORDER BY inst_name ASC";
-                    $res_clg = mysqli_query($con,$sql_clg);
-                    while($res_arr = mysqli_fetch_array($res_clg)){
-                ?>
-                <li>
-                    <div class="collapsible-header valign-wrapper js-click">
-                        <i class="material-icons">account_balance</i><span><?php echo $res_arr['inst_name']; ?></span><span> (<?php echo $res_arr['inst_code']; ?>)</span>
-                        <div class="panel_options">
-                            <input type="checkbox" id="approve">
-                            <label for="approve">Approve</label>   
-                            <button class="waves-effect waves-yellow btn yellow darken-3">EDIT</button>
-                            <button class="waves-effect waves-red btn red lighten-2">DELETE</button>
-                            <button class="waves-effect waves-blue btn blue lighten-2">PRINT</button>
-                        </div>
+            <?php
+                $sql_clg = "SELECT inst_name FROM cred ORDER BY inst_name ASC";
+                $res_clg = mysqli_query($con,$sql_clg);
+                while($res_arr = mysqli_fetch_array($res_clg)){
+            ?>
+            <div class="card-panel z-depth-2 modal-trigger" href="#modal1">
+                <div class="valign-wrapper left">
+                    <i class="material-icons">account_balance</i> <?php echo $res_arr['inst_name']; ?>
+                </div>
+                <div class="right panel_options">
+                    <input type="checkbox" id="approve">
+                    <label for="approve">Approve</label>  
+                    <button class="waves-effect waves-yellow btn yellow darken-3">EDIT</button>
+                    <button class="waves-effect waves-red btn red lighten-2">DELETE</button>
+                    <button class="waves-effect waves-blue btn blue lighten-2">PRINT</button>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+
+            <!-- Modal Structure -->
+            <div id="modal1" class="modal modal-fixed-footer">
+                <div class="modal-content">
+                    <h4>Government College Of Engineering and Textile Technology, Berhampore</h4>
+                    <hr>
+                    <div class="row">
+                        <div class="col s6">Institute ID: XXXX</div>
+                        <div class="col s6">Institute Code: XXXX</div>
                     </div>
-                    <div class="collapsible-body">
-                        
+                    <div class="row">
+                        <div class="col s6">Establishment Year: 1234</div>
+                        <div class="col s6">Accredition: NAAC-X</div>
                     </div>
-                </li>
-                <?php
-                    }
-                ?>
-                <li>
-                    <div class="collapsible-header">
-                        <i class="material-icons">account_balance</i>Government College of Engineering & Textile Technology, Berhampore
-                        <div class="panel_options">
-                            <input type="checkbox" id="approve">
-                            <label for="approve">Approve</label>   
-                            <a href="" class="waves-effect waves-yellow btn yellow darken-3">EDIT</a>
-                            <a class="waves-effect waves-red btn red lighten-2">DELETE</a>
-                            <a class="waves-effect waves-blue btn blue lighten-2">PRINT</a>
-                        </div>
+                    <div class="row">
+                        <div class="col s6">Insitute Type: XXXXXXXXXX</div>
+                        <div class="col s6">Institute Affiliation: XXXXXX</div>
                     </div>
-                    <div class="collapsible-body">
-                        <span>Lorem ipsum dolor sit amet.</span>
+                    <div class="row">
+                        <div class="col s6">Institute Approval: XXXX</div>
+                        <div class="col s6"></div>
                     </div>
-                </li>
-                <li>
-                    <div class="collapsible-header">
-                        <i class="material-icons">account_balance</i>Government College of Engineering and Leather Technology
-                        <div class="panel_options">
-                            <input type="checkbox" id="approve">
-                            <label for="approve">Approve</label>   
-                            <a href="" class="waves-effect waves-yellow btn yellow darken-3">EDIT</a>
-                            <a class="waves-effect waves-red btn red lighten-2">DELETE</a>
-                            <a class="waves-effect waves-blue btn blue lighten-2">PRINT</a>
-                        </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col s6">Institute Address: 123, XXXX</div>
+                        <div class="col s6">Pin: 1234567</div>
                     </div>
-                    <div class="collapsible-body">
-                        <span>Lorem ipsum dolor sit amet.</span>
+                    <div class="row">
+                        <div class="col s6">State: XXXXXXXX</div>
+                        <div class="col s6">District: XXXXXX</div>
                     </div>
-                </li>
-            </ul>
+                    <hr>
+                    <div class="row">
+                        <div class="col s6">Contact Number: 1234567890</div>
+                        <div class="col s6">E-Mail: XXXXXXX@XXXX.XXX</div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">Website: http://xxx.xxxxx.xxx/</div>
+                        <div class="col s6"></div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col s6">Institute Head Name: XXXXXX XXXX</div>
+                        <div class="col s6">Designation: XXX XXXX XXX</div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">Contact (Mobile): 1234567890</div>
+                        <div class="col s6">Contact (Landline): 1234567890</div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">E-Mail: xxxxxxxx@xxxx.xxx</div>
+                        <div class="col s6"></div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col s6">TPO Name: XXXXXX XXXXX</div>
+                        <div class="col s6">Contact (1): 1234567890</div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">Contact (2): 1234567890</div>
+                        <div class="col s6">E-Mail: xxxxxxxxx@xxxx.xxx</div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col s6">Total number of computers: 123</div>
+                        <div class="col s6">Total number of computer lab: 123</div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">Minimum number of computers in lab: 123</div>
+                        <div class="col s6">Internet Speed: 123456 Kbps</div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6">Total Hall capacity: 123</div>
+                        <div class="col s6">Total CCTV cameras in lab: 123</div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class="modal-action  waves-effect waves-blue btn-flat">PRINT</a>
+                    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">DONE</a>
+                </div>
+            </div>
+            <?php
+                }
+            ?>
         </div>
     </main>
 
@@ -170,6 +213,16 @@
     <script>
         $(document).ready(function () {
             $('.collapsible').collapsible();
+        });
+    </script>
+    <script>
+        $(document).ready(function () {
+            // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+            $('.modal').modal();
+            $('.panel_options').on('click', function (e) {
+                // stop the event from bubbling.
+                e.stopPropagation();
+            });
         });
     </script>
 </body>
