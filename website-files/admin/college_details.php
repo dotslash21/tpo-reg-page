377ab1bea2dc28187323db2e06a748faba5a320b
Arkadip <in2arkadipb13@gmail.com>
2018-07-31 14:19:47 +0530

    404 added

--- website-files/admin/college_details.php
+++ website-files/admin/college_details.php
@@ -201,6 +201,7 @@
     <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script src="../assets/js/admin_clg.js"></script>
     <script>
         $(".button-collapse").sideNav();
     </script>
---
+++ website-files/assets/js/admin_clg.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var r = $("input#approve");
+    r.change(function(){
+        if(r.is(":checked")){
+            console.log("ON")
+        }
+        else if(r.not(":checked")){
+            console.log("OFF");
+        }
+    })
+})
