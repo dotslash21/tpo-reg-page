29d1ef7cb6691ba4fb3f673293c00b0cbd1b59e6
Arkadip <in2arkadipb13@gmail.com>
2018-08-01 00:54:18 +0530

    working course details

--- website-files/admin/college_details.php
+++ website-files/admin/college_details.php
@@ -18,7 +18,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -71,6 +71,7 @@
             <hr>
             <br>
 
+            <!-- Print Button -->
             <div class="row">
                 <div class="col s10">
                 </div>
@@ -81,105 +82,54 @@
                 </div>
             </div>
 
+            <!-- Indivisual College Details print -->
             <?php
-                $sql_clg = "SELECT inst_name FROM cred ORDER BY inst_name ASC";
+
+                //Sql
+                $sql_clg = "SELECT inst_name,inst_code FROM cred ORDER BY inst_name ASC";
                 $res_clg = mysqli_query($con,$sql_clg);
                 while($res_arr = mysqli_fetch_array($res_clg)){
             ?>
-            <div class="card-panel z-depth-2 modal-trigger" href="#modal1">
+
+            <!-- College Cards -->
+            <div class="card-panel z-depth-2 modal-trigger" href="#modal-<?php echo $res_arr['inst_code']; ?>" id="trig-<?php echo $res_arr['inst_code']; ?>">
+
                 <div class="valign-wrapper left">
-                    <i class="material-icons">account_balance</i> <?php echo $res_arr['inst_name']; ?>
+                    <i class="material-icons">account_balance</i> <span><?php echo $res_arr['inst_name']; ?></span><span>(<?php echo $res_arr['inst_code']; ?>)</span>
                 </div>
                 <div class="right panel_options">
-                    <input type="checkbox" id="approve">
-                    <label for="approve">Approve</label>  
-                    <button class="waves-effect waves-yellow btn yellow darken-3">EDIT</button>
-                    <button class="waves-effect waves-red btn red lighten-2">DELETE</button>
-                    <button class="waves-effect waves-blue btn blue lighten-2">PRINT</button>
+                    <input type="checkbox" id="approve-<?php echo $res_arr['inst_code']; ?>" class="">
+                    <label for="approve-<?php echo $res_arr['inst_code']; ?>">Approve</label>  
+                    <button class="waves-effect waves-yellow btn yellow darken-3" id="edit-<?php echo $res_arr['inst_code']; ?>">EDIT</button>
+                    <button class="waves-effect waves-red btn red lighten-2" id="delete-<?php echo $res_arr['inst_code']; ?>">DELETE</button>
+                    <button class="waves-effect waves-blue btn blue lighten-2" id="print-<?php echo $res_arr['inst_code']; ?>">PRINT</button>
                 </div>
                 <div class="clearfix"></div>
             </div>
 
-            <!-- Modal Structure -->
-            <div id="modal1" class="modal modal-fixed-footer">
-                <div class="modal-content">
-                    <h4>Government College Of Engineering and Textile Technology, Berhampore</h4>
-                    <hr>
-                    <div class="row">
-                        <div class="col s6">Institute ID: XXXX</div>
-                        <div class="col s6">Institute Code: XXXX</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Establishment Year: 1234</div>
-                        <div class="col s6">Accredition: NAAC-X</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Insitute Type: XXXXXXXXXX</div>
-                        <div class="col s6">Institute Affiliation: XXXXXX</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Institute Approval: XXXX</div>
-                        <div class="col s6"></div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col s6">Institute Address: 123, XXXX</div>
-                        <div class="col s6">Pin: 1234567</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">State: XXXXXXXX</div>
-                        <div class="col s6">District: XXXXXX</div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col s6">Contact Number: 1234567890</div>
-                        <div class="col s6">E-Mail: XXXXXXX@XXXX.XXX</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Website: http://xxx.xxxxx.xxx/</div>
-                        <div class="col s6"></div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col s6">Institute Head Name: XXXXXX XXXX</div>
-                        <div class="col s6">Designation: XXX XXXX XXX</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Contact (Mobile): 1234567890</div>
-                        <div class="col s6">Contact (Landline): 1234567890</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">E-Mail: xxxxxxxx@xxxx.xxx</div>
-                        <div class="col s6"></div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col s6">TPO Name: XXXXXX XXXXX</div>
-                        <div class="col s6">Contact (1): 1234567890</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Contact (2): 1234567890</div>
-                        <div class="col s6">E-Mail: xxxxxxxxx@xxxx.xxx</div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col s6">Total number of computers: 123</div>
-                        <div class="col s6">Total number of computer lab: 123</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Minimum number of computers in lab: 123</div>
-                        <div class="col s6">Internet Speed: 123456 Kbps</div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6">Total Hall capacity: 123</div>
-                        <div class="col s6">Total CCTV cameras in lab: 123</div>
-                    </div>
+            <!-- All college Modal Structure -->
+            <div id="modal-<?php echo $res_arr['inst_code']; ?>" class="modal modal-fixed-footer" >
+
+                <div class="modal-content" id="modalcon-<?php echo $res_arr['inst_code']; ?>">
                 </div>
+
                 <div class="modal-footer">
-                    <a href="#!" class="modal-action  waves-effect waves-blue btn-flat">PRINT</a>
-                    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">DONE</a>
+                    <a href="javascript: void(0);" class="modal-action  waves-effect waves-blue btn-flat" id="print-<?php echo $res_arr['inst_code']; ?>">PRINT</a>
+                    <a href="javascript: void(0);" class="modal-action modal-close waves-effect waves-green btn-flat">DONE</a>
                 </div>
             </div>
+
+            <!-- script for value -->
+            <script>
+                $(document).ready(function () {
+                    $("#trig-<?php echo $res_arr['inst_code']; ?>").click(function(){
+                        $.get("../ajax/admin_college_indv.php?inst_code=<?php echo $res_arr['inst_code']; ?>", function(data){
+                            $("#modalcon-<?php echo $res_arr['inst_code']; ?>").append(data.value);
+                        }, "json")
+                    })
+                })
+            </script>
+            
             <?php
                 }
             ?>
@@ -198,7 +148,6 @@
 
 
     <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <script src="../assets/js/admin_clg.js"></script>
     <script>
