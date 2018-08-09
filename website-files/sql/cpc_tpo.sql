f018886debc484445d4fc33236b8e0bc38f25663
Arkadip <in2arkadipb13@gmail.com>
2018-08-09 15:29:49 +0530

    Added new sql

--- website-files/sql/cpc_tpo.sql
+++ website-files/sql/cpc_tpo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2018 at 05:27 PM
+-- Generation Time: Aug 09, 2018 at 03:28 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -62,7 +62,49 @@ INSERT INTO `college_crs` (`college_id`, `deg_optd`, `intake`) VALUES
 (111, 'B.Sc - CS', 0),
 (442, 'B.Tech - CSE', 884),
 (442, 'B.Tech - TT', 554),
-(442, 'B.Tech - EE', 111);
+(442, 'B.Tech - EE', 111),
+(1999, 'M.Tech - EE', 78),
+(1999, 'B.Tech - TT', 55),
+(1999, 'B.Tech - EE', 55),
+(1999, 'B.Tech - EE', 22),
+(123459, 'M.Tech - EE', 99),
+(123459, 'B.Tech - CSE', 65),
+(123459, 'B.Tech - EE', 22),
+(9876, 'M.Tech - EE', 556),
+(9876, 'B.Tech - TT', 66),
+(9876, 'B.Tech - EE', 77),
+(12377, 'B.Tech - CSE', 12),
+(12377, 'B.Tech - TT', 13),
+(112233, 'M.Tech - EE', 99),
+(112233, 'B.Tech - CSE', 88),
+(112233, 'B.Tech - TT', 55),
+(12121, 'M.Tech - EE', 13),
+(12121, 'B.Tech - TT', 14),
+(12121, 'B.Tech - EE', 15),
+(12312, 'B.Tech - CSE', 99),
+(12312, 'B.Tech - TT', 55),
+(98789, 'M.Tech - EE', 12),
+(98789, 'B.Tech - CSE', 13),
+(98789, 'B.Tech - EE', 14),
+(98789, 'B.Tech - EE', 15),
+(111222, 'M.Tech - EE', 12),
+(111222, 'B.Tech - CSE', 13),
+(111222, 'B.Tech - EE', 14),
+(111222, 'B.Tech - EE', 15),
+(21231, 'M.Tech - EE', 23),
+(21231, 'B.Tech - TT', 11),
+(1235465, 'M.Tech - EE', 77),
+(1111111111, 'M.Tech - EE', 87),
+(1324567896, 'M.Tech - EE', 99),
+(2147483647, 'M.Tech - EE', 55),
+(1238, 'M.Tech - EE', 56),
+(1238, 'M.Tech - CSE', 67),
+(1234567, 'M.Tech - EE', 88),
+(1234567, 'B.Tech - EE', 55),
+(123456, 'M.Tech - EE', 888),
+(12314, 'M.Tech - EE', 7),
+(152353, 'M.Tech - EE', 9),
+(35535, 'B.Tech - CSE', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -87,7 +129,9 @@ INSERT INTO `course_list` (`id`, `degree`, `course_name`) VALUES
 (6, 'M.Tech', 'EE'),
 (7, 'B.Sc', 'CS'),
 (9, 'B.Tech', 'EE'),
-(10, 'B.Tech', 'EE');
+(10, 'B.Tech', 'EE'),
+(11, 'M.Tech', 'CSE'),
+(12, 'BCA', 'bachelor in Computer Application');
 
 -- --------------------------------------------------------
 
@@ -96,52 +140,57 @@ INSERT INTO `course_list` (`id`, `degree`, `course_name`) VALUES
 --
 
 CREATE TABLE `cred` (
-  `id` int(11) NOT NULL,
-  `inst_name` varchar(100) NOT NULL,
-  `inst_code` int(11) NOT NULL,
-  `uid` varchar(50) NOT NULL,
-  `pwd` varchar(50) NOT NULL,
-  `estd` int(11) NOT NULL,
-  `inst_accrd` varchar(20) NOT NULL,
-  `inst_type` varchar(20) NOT NULL,
-  `inst_affl` varchar(50) NOT NULL,
-  `inst_aprv` varchar(50) NOT NULL,
-  `state` varchar(20) NOT NULL,
-  `district` varchar(20) NOT NULL,
-  `pin` int(11) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `website` varchar(50) NOT NULL,
-  `head_name` varchar(50) NOT NULL,
-  `inst_headdesg` varchar(50) NOT NULL,
-  `head_contact` int(11) NOT NULL,
-  `head_mob` int(11) NOT NULL,
-  `head_email` varchar(50) NOT NULL,
-  `tpo_name` varchar(50) NOT NULL,
-  `tpo_ph` int(11) NOT NULL,
-  `tpo_ph2` int(11) NOT NULL,
-  `tpo_email` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL COMMENT 'User resigtration id',
+  `inst_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute Name',
+  `inst_code` int(11) NOT NULL COMMENT 'institute Code',
+  `uid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute User Id',
+  `pwd` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Password',
+  `estd` int(8) NOT NULL COMMENT 'Institute ESTD',
+  `inst_accrd` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute Accriditation',
+  `inst_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Type',
+  `inst_affl` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Affliation',
+  `inst_aprv` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Approval',
+  `state` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute State',
+  `district` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute District',
+  `pin` int(8) NOT NULL COMMENT 'institute PIN',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Address',
+  `phone` int(11) NOT NULL COMMENT 'institute Phone No',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Email',
+  `website` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Website',
+  `head_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT 'institute Head Name',
+  `inst_headdesg` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute Head Designation',
+  `head_contact` int(11) NOT NULL COMMENT 'institute Head Phone',
+  `head_mob` int(11) NOT NULL COMMENT 'Institute Head Mobile',
+  `head_email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute Head Email',
+  `tpo_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'institute TPO name',
+  `tpo_ph` int(11) NOT NULL COMMENT 'institute TPO Phone',
+  `tpo_ph2` int(11) NOT NULL COMMENT 'institute TPO Phone 2',
+  `tpo_email` varchar(50) NOT NULL COMMENT 'institute TPO Email',
   `no_of_comp` int(11) NOT NULL,
   `num_cmplab` int(10) NOT NULL,
   `min_num_cmp` int(10) NOT NULL,
   `int_speed` int(11) NOT NULL,
   `hall_cap` int(11) NOT NULL,
-  `fibop_lan` tinyint(1) NOT NULL,
-  `cctv_no` int(11) NOT NULL
+  `fibop_lan` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Instititute fiberlab',
+  `cctv_no` int(11) NOT NULL,
+  `upload` varchar(40) NOT NULL COMMENT 'institute Uploadfile name',
+  `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Registration Time',
+  `browser` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute registered browser',
+  `inst_ip` varchar(64) NOT NULL COMMENT 'Institute registered Ip',
+  `inst_approve` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute Approval',
+  `inst_delete` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Institute Delete'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cred`
 --
 
-INSERT INTO `cred` (`id`, `inst_name`, `inst_code`, `uid`, `pwd`, `estd`, `inst_accrd`, `inst_type`, `inst_affl`, `inst_aprv`, `state`, `district`, `pin`, `address`, `phone`, `email`, `website`, `head_name`, `inst_headdesg`, `head_contact`, `head_mob`, `head_email`, `tpo_name`, `tpo_ph`, `tpo_ph2`, `tpo_email`, `no_of_comp`, `num_cmplab`, `min_num_cmp`, `int_speed`, `hall_cap`, `fibop_lan`, `cctv_no`) VALUES
-(1, 'Anshumali Shaw', 231, 'qweqw213', 'e2q3eq32e3', 31412, 'NAAC-A+', 'Government', 'MAKAUT', '2', 'West Bengal (WB)', 'Birbhum', 311230, 'ewrewdfcs', 231312312, 'fewf@Wdfsd.vgdf', 'http://wwww.hdjh.dfd', 'dsfasfasdf sdfasdfgas', 'sdfasdfsdf', 2147483647, 342134132, '341GSDFGS@Wdfdsfsd.gd', 'safgargargae ds', 2132143214, 2147483647, 'weerewr@guk.fd', 9999, 999, 1, 2147483647, 999, 0, 0),
-(2, 'GCETTB', 111, 'gcettb1', 'gcettb1', 2018, 'NAAC-A', 'Government', 'MAKAUT', '1', 'West Bengal (WB)', 'Murshidabad', 432434, 'asdfg', 2147483647, 'shaw.anshu@gmail.com', 'http://www.gcettb.ac.in', 'dsfasfasdf sdfasdfgas', 'sdfasdfsdf', 8, 7, '341GSDFGS@Wdfdsfsd.gd', 'qwertyuion asdfgh', 5, 5, 'qwertyu@asdgfh.com', 80, 5, 20, 1000, 4, 20, 0),
-(3, '', 0, '', '', 0, '', '', '', '', '', '', 0, '', 0, '', '', '', '', 0, 0, '', '', 0, 0, '', 0, 0, 0, 0, 0, 0, 0),
-(4, 'Arkadip Bhattacharya', 123456, '123', '123456789', 1800, 'NAAC-A', 'Government-Aided', 'MAKAUT', '3', 'Andhra Pradesh (AP)', 'llhkghjfgdh', 713144, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'in2arkadipb13@gmail.com', 'http://timeclock.jk', 'Arkadip Bhattacharya', 'nxdn', 123456789, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Road,Gorabazar', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 12, 13, 14, 15, 16, 0, 17),
-(13, 'qwertyuiop', 123, '1234', '1234567890', 1800, 'NAAC-A+', 'Government', 'MAKAUT', '1', 'Arunachal Pradesh (A', 'asasasasa', 713144, 'Mankar', 2147483647, 'jonney.vaai@gmail.com', 'http://timeclock.jj', 'hi haallo', 'nxdnoo', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Ro', 2147483647, 2147483647, 'james@gmail.com', 87, 45, 78, 45, 21, 0, 65),
-(14, 'qwertyyuioasd', 442, '12432', '123456789', 1800, 'NAAC-A+', 'Government', 'MAKAUT', '1', 'Andhra Pradesh (AP)', 'llhkghjfgdh', 742101, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'dhrthsrt3@gmail.com', 'http://timeclock.jl', 'azsxdcfv', 'aqswdefr', 2147483647, 2147483647, 'aaaaaaaa@gmail.com', 'cdvfbgnhjm', 2147483647, 2147483647, 'sssqwer@gmail.com', 985, 442, 274, 22, 441, 0, 774);
+INSERT INTO `cred` (`id`, `inst_name`, `inst_code`, `uid`, `pwd`, `estd`, `inst_accrd`, `inst_type`, `inst_affl`, `inst_aprv`, `state`, `district`, `pin`, `address`, `phone`, `email`, `website`, `head_name`, `inst_headdesg`, `head_contact`, `head_mob`, `head_email`, `tpo_name`, `tpo_ph`, `tpo_ph2`, `tpo_email`, `no_of_comp`, `num_cmplab`, `min_num_cmp`, `int_speed`, `hall_cap`, `fibop_lan`, `cctv_no`, `upload`, `reg_time`, `browser`, `inst_ip`, `inst_approve`, `inst_delete`) VALUES
+(37, 'Arkadip Bhattacharywett', 1234567, '32638538', '$2y$10$S9kgTrJ0ElnePgKM1km3Ue2FitXBKdizSxu9puQp5tX', 1800, 'NAAC-A+', 'Government', 'JU', '1', 'Arunachal Pradesh (A', 'llhkghjfgdh', 713144, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'in2arkadipb13@gmail.com', 'http://timeclock.jk', 'Arkadip Bhattacharya', 'qwerty', 1234567788, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Road,Gorabazar', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 88, 8875, 224, 955, 896, '0', 888, '', '2018-08-08 16:41:40', '', '', '', ''),
+(38, 'Arkadip Bhattacharya', 123456, '123', '$2y$10$JO0dAUvzHb2oNAK9GonDn.5533GFcCPRTm3eVj3lcSK', 1800, 'NAAC-A+', 'Government', 'JU', '2', 'Andhra Pradesh (AP)', 'llhkghjfgdh', 713144, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'in2arkadipb13@gmail.com', 'http://timeclock.pl', 'Arkadip Bhattacharya', 'nxdn', 1234567890, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Road,Gorabazar', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 6, 5, 997, 444, 555, 'yes', 444, '', '2018-08-08 16:41:40', '', '', '', ''),
+(42, 'Arkadip Bhattacharyaaar', 12314, '114', '$2y$10$V4m8BBHfxiP5FyQGHHeHvO7Gu99JfzdYm7V.spm97Hj', 1800, 'NAAC-A+', 'Government-Aided', 'Maulana Kalam Azad University of Technology', 'UGc', 'Andhra Pradesh (AP)', 'llhkghjfgdh', 713144, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'in2arkadipb13@gmail.com', 'http://timeclock.jk', 'Arkadip Bhattacharya', 'nxdn', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Road,Gorabazar', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 1123, 241, 2332, 235235, 2342, 'yes', 1242, '', '2018-08-08 16:54:07', '', '', '', ''),
+(43, '1ahgshf', 152353, '2525325', '$2y$10$q2ArRxgwoqlYMAu7bGJBEuOWW3oHT1B.LyPgDlrpDwB', 1800, 'NAAC-A+', 'Government', 'Maulana Kalam Azad University of Technology', 'UGc', 'Andhra Pradesh (AP)', 'asasasasa', 742101, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'in2arkadipb13@gmail.com', 'http://timeclock.pl', 'Arkadip Bhattacharya', 'nxdn', 1233456678, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Road,Gorabazar', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 98, 44, 22, 55, 54, 'yes', 5, '152353.jpg', '2018-08-08 17:01:27', '', '', '', ''),
+(44, 'Arkadip Bhattacharya131', 35535, '12345678', '$2y$10$eKMimiwhnuztWXBee0GCZe5gpaiJrDFuTf.avwqEtDr', 1800, 'NAAC-A+', 'Government', 'Jadavpur University', 'AICTE', 'Andhra Pradesh (AP)', 'llhkghjfgdh', 742101, '7/c\nLaxmi Prasad Singh Road,Gorabazar', 2147483647, 'in2arkadipb13@gmail.com', 'http://timeclock.jk', 'Arkadip Bhattacharya', 'nxdn', 1234569987, 2147483647, 'in2arkadipb13@gmail.com', 'Laxmi Prasad Singh Road,Gorabazar', 2147483647, 2147483647, 'in2arkadipb13@gmail.com', 787, 98, 546, 544, 554, 'yes', 797, '', '2018-08-09 09:07:55', '::1', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -170,13 +219,13 @@ ALTER TABLE `cred`
 -- AUTO_INCREMENT for table `course_list`
 --
 ALTER TABLE `course_list`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `cred`
 --
 ALTER TABLE `cred`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'User resigtration id', AUTO_INCREMENT=45;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
