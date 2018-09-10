6d308328054f45627d1c9486ea4141f8a2ad72b8
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-09-10 19:53:25 +0530

    auto update active_status in notices page is done

--- website-files/ajax/notices.php
+++ website-files/ajax/notices.php
@@ -12,25 +12,30 @@
 
                 $return = [];
 
-                $value = '';
-                //Sql for getting data
-                $sql = "SELECT title, content, publish_date, expiry_date, file_name FROM `notices` ORDER BY expiry_date DESC";
-                $result= mysqli_query($con, $sql);
-                while($result_arr = mysqli_fetch_array($result)){
+                //Notice value change                
+                $current_time = time();
+                $sql_update = "UPDATE `notices` SET active_status = 0 WHERE expiry_date < ".$current_time."";
+                if(mysqli_query($con, $sql_update)){
+                    $value = '';
+                    //Sql for getting data
+                    $sql = "SELECT title, content, publish_date, expiry_date, file_name FROM `notices` ORDER BY expiry_date DESC";
+                    $result= mysqli_query($con, $sql);
+                    while($result_arr = mysqli_fetch_array($result)){
 
-                    $pub_date = date("M. d, Y", $result_arr['publish_date']);
-                    $exp_date = date("M. d, Y", $result_arr['expiry_date']);
+                        $pub_date = date("M. d, Y", $result_arr['publish_date']);
+                        $exp_date = date("M. d, Y", $result_arr['expiry_date']);
 
-                    $value .= "<tr>";
-                    $value .=   "<td>".$result_arr['title']."</td>";
-                    $value .=   "<td>".$pub_date."</td>";
-                    $value .=   "<td>".$exp_date."</td>";
-                    $value .=   "<td>".$result_arr['content']."</td>";
-                    $value .=   "<td>";
-                    $value .=       "<a href=\"./upload/notice/".$result_arr['file_name']."\" class=\"btn btn-info btn-xs\">";
-                    $value .=       "<span class=\"glyphicon glyphicon-download-alt\"></span>Download</a>";
-                    $value .=   "</td>";
-                    $value .=   "</tr>";
+                        $value .= "<tr>";
+                        $value .=   "<td>".$result_arr['title']."</td>";
+                        $value .=   "<td>".$pub_date."</td>";
+                        $value .=   "<td>".$exp_date."</td>";
+                        $value .=   "<td>".$result_arr['content']."</td>";
+                        $value .=   "<td>";
+                        $value .=       "<a href=\"./upload/notice/".$result_arr['file_name']."\" class=\"btn btn-info btn-xs\">";
+                        $value .=       "<span class=\"glyphicon glyphicon-download-alt\"></span>Download</a>";
+                        $value .=   "</td>";
+                        $value .=   "</tr>";
+                    }
                 }
 
                 $return['notices'] = $value;
