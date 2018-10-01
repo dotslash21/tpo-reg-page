9d0e60aa7712a4dfa5b39119e130ae9021684488
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-01 15:28:39 +0530

    print all file uploaded

--- website-files/ajax/print_all.php
+++ website-files/ajax/print_all.php
@@ -11,12 +11,25 @@
                 require '../inc/func.php';
 
                 $token = $_POST['token'];
+                $degree = $_POST['degree'];
+                $course = $_POST['course'];
                 if(XCSRF::varifycsrf('ad-clg-det',$token)){
     
                     //Always return Json format
                     header('Content-Type: application/json');
 
                     $return['done'] = XCSRF::mkcsrf('ad-print-all');
+
+                    if($degree == 'all'){
+                        $de_return = "degree[]=". $degree;
+                    }
+                    else {
+                        $de_count = count($degree);
+                        $de_return = '';
+                        for ($i=0; $i < $de_count; $i++) { 
+                            $de_return .= "degree[]=". $degree[$i];
+                        }
+                    }
 
                     $return['degree'] = '';
                     $return['course'] = '';
