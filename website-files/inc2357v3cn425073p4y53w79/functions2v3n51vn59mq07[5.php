f1e5cfd8327b536b03d4be4a62b275d9abe3455e
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-02 17:17:13 +0530

    getip area changed

--- website-files/inc2357v3cn425073p4y53w79/functions2v3n51vn59mq07[5.php
+++ website-files/inc2357v3cn425073p4y53w79/functions2v3n51vn59mq07[5.php
@@ -1,10 +1,40 @@
 <?php
 
+    /**
+     * 
+     * File for All Functions
+     * 
+     * Created by IntelliJ IDEA.
+     * User: Arkadip
+     * Date: 10/02/18
+     * Time: 05:01 PM 
+    */
+
 if(defined('_functionsqn72v3[701v[c124[m1c')){
 
     //Functions for cleaning the request items
     function clean($con, $var){
         return mysqli_real_escape_string($con, htmlspecialchars($var,ENT_QUOTES));
+    }
+
+    //Function for getting users ip
+    function get_client_ip() {
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
     }
 }
 
