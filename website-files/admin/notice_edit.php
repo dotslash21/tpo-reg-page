555aa41f20978087201ec3474165952039db5b0b
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 23:29:49 +0530

    buug fixed

--- website-files/admin/notice_edit.php
+++ website-files/admin/notice_edit.php
@@ -19,7 +19,7 @@
                 //id is there
                 $id = Filter::Int(clean($_GET['n_id']));
 
-                $smt_result = $pdocon->prepare("SELECT * FROM `notices` WHERE `sl_no` = '".$id."' ");
+                $smt_result = $pdocon->prepare("SELECT * FROM `notices` WHERE `sl_no` = :id ");
                 $smt_result->bindParam(':id',$id,PDO::PARAM_STR);
                 $smt_result->execute();
                 $result_arr = $smt_result->fetch(PDO::FETCH_ASSOC);
