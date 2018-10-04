ac0e0a56c1c55c02fc1aa143ed5c39108204d8b4
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 21:27:16 +0530

    notices uses pdo

--- website-files/ajax/notices.php
+++ website-files/ajax/notices.php
@@ -14,17 +14,19 @@
 
                 //Notice value change                
                 $current_time = time();
-                $sql_update = "UPDATE `notices` SET active_status = 0 WHERE expiry_date < ".$current_time."";
-                if(mysqli_query($con, $sql_update)){
+
+                $smt_update = $pdocon->prepare("UPDATE `notices` SET active_status = 0 WHERE expiry_date < :current_time");
+                if($smt_update->execute(array(':current_time'=>$current_time))){
                     $value = '';
                     //Sql for getting data
-                    $sql = "SELECT title, content, publish_date, expiry_date, file_name FROM `notices` ORDER BY expiry_date DESC";
-                    $result= mysqli_query($con, $sql);
-                    while($result_arr = mysqli_fetch_array($result)){
+                    $stmt = $pdocon->prepare("SELECT title, content, publish_date, expiry_date, file_name FROM `notices` ORDER BY expiry_date DESC");
+                    $stmt->execute();
+                    while($result_arr = $stmt->fetch(PDO::FETCH_ASSOC)){
 
                         $pub_date = date("M. d, Y", $result_arr['publish_date']);
                         $exp_date = date("M. d, Y", $result_arr['expiry_date']);
 
+                        //Check for file exists
                         if(strlen($result_arr['file_name']) < 1 ){
                             $des = "disabled";
                         }
