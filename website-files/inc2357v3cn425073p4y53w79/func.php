7d9a3fc2b1a031e47ebd0fd0cc1c15623dca54da
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 19:57:38 +0530

    filter class added

--- website-files/inc2357v3cn425073p4y53w79/func.php
+++ website-files/inc2357v3cn425073p4y53w79/func.php
@@ -21,6 +21,7 @@
         define('_adminnb387w6[vn4', true);
 
         require_once INC.'/classes124vq32524s/DB2vq95mn42vnm704.php';       //PDO DB class
+        require_once INC.'/classes124vq32524s/filter2vc63p5th.php';         //Filter class
         require_once INC.'/classes124vq32524s/XCSRFgrwhgr.php';             //XCSRF class
         require_once INC.'/classes124vq32524s/admin2v645n317vc05.php';      //admin
         require_once INC.'/db-con2vpn2b569vn.php';                          //mysqli db
