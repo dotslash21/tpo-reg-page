01568eb9fd0e5de75122f3929f65c1f34fc4cc4b
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 23:10:14 +0530

    admin login is changed

--- website-files/ajax/admin_login.php
+++ website-files/ajax/admin_login.php
@@ -34,30 +34,31 @@
                 if($respone->success){      //Checks the  success parameter
                     //If not a robot(varified by captcha)
 
-                    date_default_timezone_set("Asia/Calcutta");
                     $current_time = date("Y/M/d g:i:s A T P");
                     $current_ip = get_client_ip();
 
                     //Escaping variables
-                    $id_clean = clean($id);
-                    $password_clean = clean($password);
+                    $id_clean = Filter::String(clean($id));
+                    $password_clean = Filter::String(clean($password));
 
                     //Check if admin already exist or not
-                    $query_admin = "SELECT id,password FROM admins WHERE id ='".$id_clean."' LIMIT 1";
-                    $result_admin =  mysqli_query($con,$query_admin);
-                    if(mysqli_num_rows($result_admin) > 0){
+                    $smt_admin = $pdocon->prepare("SELECT id, password FROM admins WHERE id =:id LIMIT 1");
+                    $smt_admin->bindParam(':id', $id_clean, PDO::PARAM_STR);
+                    $smt_admin->execute();
+                    if($smt_admin->rowCount() > 0){
                         //Admin exists
-                        $admin_details = mysqli_fetch_array($result_admin);
-                        $admin_id = $admin_details['id'];
-                        $hash = $admin_details['password'];
+                        $admin_details  = $smt_admin->fetch(PDO::FETCH_ASSOC);
+                        $admin_id       = $admin_details['id'];
+                        $hashed         = $admin_details['password'];
 
-                        if(password_verify($password_clean,$hash)){
+                        if(password_verify($password_clean,$hashed)){
                             //User get signed in
-                            $query_add_val= "UPDATE admins SET Last_login_time= '".$current_time."', Last_login_ip = '".$current_ip."' WHERE id = '".$admin_id."' ";
-                            if(mysqli_query($con, $query_add_val)){
+
+                            $smt_add_val = $pdocon->prepare("UPDATE admins SET Last_login_time= :current_time, Last_login_ip = :current_ip WHERE id = :id ");
+                            if($smt_add_val->execute(array(':current_time'=>$current_time,':current_ip'=>$current_ip,':id'=>$admin_id))){
                                 $return['redirect'] = './dashboard.php';
                                 $_SESSION['admin_name'] = $admin_id;
-                                $_SESSION['admin_id'] = hash('SHA256',' _!@wrwe12vdqsx 31 242 qaz'. $admin_id . 'za131 '.time());
+                                $_SESSION['admin_id'] = hash('SHA512',' _!@wrwe12vdqsx 31 242 qaz'. $admin_id . 'za131 '.time());
                                 setcookie("_t", hash('sha512', $_SERVER['HTTP_USER_AGENT'] . ' asdqwgfgabhg efaoyaffrgerhehe' . $_SESSION['admin_id'] . 'kKJDNFO3J@qwwe135*&^12322' .time() . '62n663nbsbrbyeby' . get_client_ip()), 0, "/", null, false, true);
                             }
                         }
