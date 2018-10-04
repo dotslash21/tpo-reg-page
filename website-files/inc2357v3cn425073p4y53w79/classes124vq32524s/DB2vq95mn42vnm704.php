6c62a664fcfe9cab2a16ae5fbb8e49619b77af9f
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 19:09:52 +0530

    completed the pdo connection file

--- website-files/inc2357v3cn425073p4y53w79/classes124vq32524s/DB2vq95mn42vnm704.php
+++ website-files/inc2357v3cn425073p4y53w79/classes124vq32524s/DB2vq95mn42vnm704.php
@@ -3,19 +3,26 @@
     //IF there is no constant defined called _CONFIG_, do not load file
     if(defined('_CON1swys135em856uv346_')){
 
-            //Add the config file
-            define('_CONFIG46b3b2a6_',true);
+        //Add the config file
+        define('_CONFIG46b3b2a6_',true);
 
-            // Adding the Config file for Database
-            require INC.'/../db-config.php';
+        define('HOMEDIR', dirname(dirname(dirname(__FILE__))));
 
-            if(!defined('DB_NAME') && !defined('DB_USER') && !defined('DB_PASSWORD') && !defined('DB_HOST')){
-                die('Can.t connect. Define all db connection variables to link');
-            }
+        //Check if the file exists
+        if(!file_exists(HOMEDIR.'/db-config.php')){
+            die('db-config file not found. Recheck that all files are there');
+        }
 
-            $dsn = 'mysql:charset=utf8mb4;dbname='.DB_NAME.';host='.DB_HOST.';port=3306;';
-            $user = DB_USER;
-            $pass = DB_PASSWORD;
+        // Adding the Config file for Database
+        require HOMEDIR.'/db-config.php';
+
+        //Check for all connection variables
+        if(!defined('DB_NAME') && !defined('DB_USER') && !defined('DB_PASSWORD') && !defined('DB_HOST')){
+            die('Can.t connect. Define all db connection variables to link');
+        }
+        $dsn = 'mysql:charset=utf8mb4;dbname='.DB_NAME.';host='.DB_HOST.';port=3306;';
+        $user = DB_USER;
+        $pass = DB_PASSWORD;
 
 
         class DB{
