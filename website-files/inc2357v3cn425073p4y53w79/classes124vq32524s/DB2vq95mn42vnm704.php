bc6e3449e60153e58418068ba271c72c66bf6069
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 19:27:37 +0530

    pdo activated

--- website-files/inc2357v3cn425073p4y53w79/classes124vq32524s/DB2vq95mn42vnm704.php
+++ website-files/inc2357v3cn425073p4y53w79/classes124vq32524s/DB2vq95mn42vnm704.php
@@ -3,33 +3,13 @@
     //IF there is no constant defined called _CONFIG_, do not load file
     if(defined('_CON1swys135em856uv346_')){
 
-        //Add the config file
-        define('_CONFIG46b3b2a6_',true);
-
-        define('HOMEDIR', dirname(dirname(dirname(__FILE__))));
-
-        //Check if the file exists
-        if(!file_exists(HOMEDIR.'/db-config.php')){
-            die('db-config file not found. Recheck that all files are there');
-        }
-
-        // Adding the Config file for Database
-        require HOMEDIR.'/db-config.php';
-
-        //Check for all connection variables
-        if(!defined('DB_NAME') && !defined('DB_USER') && !defined('DB_PASSWORD') && !defined('DB_HOST')){
-            die('Can.t connect. Define all db connection variables to link');
-        }
-        $dsn = 'mysql:charset=utf8mb4;dbname='.DB_NAME.';host='.DB_HOST.';port=3306;';
-        $user = DB_USER;
-        $pass = DB_PASSWORD;
 
 
         class DB{
             
             protected static $con;
 
-            private function __construct(){
+            private function __construct($dsn,$user,$pass){
 
                 try {
                     self::$con = new PDO($dsn,$user,$pass);
@@ -46,7 +26,25 @@
 
                 //If this instance was not been started, start it.
                 if (!self::$con) {
-                    new DB();
+
+                    define('_CONFIG46b3b2a6_',true);
+                    define('HOMEDIR', dirname(dirname(dirname(__FILE__))));
+    
+                    if(!file_exists(HOMEDIR.'/db-config.php')){                             //Check if the file exists
+                        die('db-config file not found. Recheck that all files are there');
+                    }
+    
+                    require HOMEDIR.'/db-config.php';                                           // Adding the Config file for Database
+    
+                    if(!defined('DB_NAME') && !defined('DB_USER') && !defined('DB_PASSWORD') && !defined('DB_HOST')){
+                        die('Can.t connect. Define all db connection variables to link');                                   //Check for all connection variables
+                    }
+                    
+                    $dsn = 'mysql:charset=utf8mb4;dbname='.DB_NAME.';host='.DB_HOST.';port=3306;';
+                    $user = DB_USER;
+                    $pass = DB_PASSWORD;
+
+                    new DB($dsn, $user, $pass);
                 }
 
                 //Return the writteable db connection
