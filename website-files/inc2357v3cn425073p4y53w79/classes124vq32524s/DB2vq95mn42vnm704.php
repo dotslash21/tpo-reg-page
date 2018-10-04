e8df97d44837039c85ac626fc89a6007160337f0
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2018-10-04 19:05:08 +0530

    pdo connection variables init

--- website-files/inc2357v3cn425073p4y53w79/classes124vq32524s/DB2vq95mn42vnm704.php
+++ website-files/inc2357v3cn425073p4y53w79/classes124vq32524s/DB2vq95mn42vnm704.php
@@ -1,37 +1,53 @@
 <?php
 
     //IF there is no constant defined called _CONFIG_, do not load file
-    if(!defined('_CONFIG_')){
+    if(defined('_CON1swys135em856uv346_')){
+
+            //Add the config file
+            define('_CONFIG46b3b2a6_',true);
+
+            // Adding the Config file for Database
+            require INC.'/../db-config.php';
+
+            if(!defined('DB_NAME') && !defined('DB_USER') && !defined('DB_PASSWORD') && !defined('DB_HOST')){
+                die('Can.t connect. Define all db connection variables to link');
+            }
+
+            $dsn = 'mysql:charset=utf8mb4;dbname='.DB_NAME.';host='.DB_HOST.';port=3306;';
+            $user = DB_USER;
+            $pass = DB_PASSWORD;
+
+
+        class DB{
+            
+            protected static $con;
+
+            private function __construct(){
+
+                try {
+                    self::$con = new PDO($dsn,$user,$pass);
+                    self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+                    self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
+                }
+                catch (PDOException $e) {
+                    echo "Could not connect to database.";
+                    exit;
+                }    
+            }
+
+            public static function getConnection() {
+
+                //If this instance was not been started, start it.
+                if (!self::$con) {
+                    new DB();
+                }
+
+                //Return the writteable db connection
+                return self::$con;
+            }
+        }
+    }
+    else {
         header('Location: .../404.php'); 
     }
-    
-    class DB{
-        
-        protected static $con;
-
-        private function __construct(){
-
-            try {
-                self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_system', 'root', '' );
-                self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-                self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
-            }
-            catch (PDOException $e) {
-                echo "Could not connect to database.";
-                exit;
-            }    
-        }
-
-        public static function getConnection() {
-
-            //If this instance was not been started, start it.
-            if (!self::$con) {
-                new DB();
-            }
-
-            //Return the writteable db connection
-            return self::$con;
-        }
-    }
-    
 ?>
--- website-files/inc2357v3cn425073p4y53w79/func.php
+++ website-files/inc2357v3cn425073p4y53w79/func.php
@@ -13,15 +13,18 @@
 
         error_reporting(0);
 
+        define('INC',dirname(__FILE__));
+
         define('_FUNCrqwetwegww', true);
         define('_CON1swys135em856uv346_',true);
         define('_functionsqn72v3[701v[c124[m1c',true);
         define('_adminnb387w6[vn4', true);
 
-        require_once dirname( __FILE__ ).'/classes124vq32524s/XCSRFgrwhgr.php';
-        require_once dirname( __FILE__ ).'/classes124vq32524s/admin2v645n317vc05.php';
-        require_once dirname( __FILE__ ).'/db-con2vpn2b569vn.php';
-        require_once dirname( __FILE__ ).'/functions2v3n51vn59mq07[5.php';
+        require_once INC.'/classes124vq32524s/DB2vq95mn42vnm704.php';       //PDO DB class
+        require_once INC.'/classes124vq32524s/XCSRFgrwhgr.php';             //XCSRF class
+        require_once INC.'/classes124vq32524s/admin2v645n317vc05.php';      //admin
+        require_once INC.'/db-con2vpn2b569vn.php';                          //mysqli db
+        require_once INC.'/functions2v3n51vn59mq07[5.php';                  //functions file
     }
 
     
